baefe7370fb33e99d29b1dde44cba542d4693834
dab <dbraband@gmail.com>
2012-05-07 19:59:40 +0200

    Adding semicolons and removing trailing newlines.

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
-var express = require('express')
-    app = express.createServer()
+/*jslint node: true, sloppy: true, indent: 4 */
+
+var express = require('express'),
+    app = express.createServer();
 
 /*
  * Configuration
  */
-app.configure(function() {
-    app.use(express.static(__dirname + '/public'))
-    app.use(express.bodyParser())
-    app.use(express.methodOverride())
-})
+app.configure(function () {
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+});
 
-app.configure('development', function() {
-    app.use(express.logger())
+app.configure('development', function () {
+    app.use(express.logger());
     app.use(express.errorHandler({
         dumpExceptions: true,
         showStack: true
-    }))
-})
+    }));
+});
 
 
-app.get('/', function(req, res) {
-    res.send('Hello World!')
-})
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
 
-app.listen(process.env.PORT || 1982)
+app.listen(process.env.PORT || 1982);
